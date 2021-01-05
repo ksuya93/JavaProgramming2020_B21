bcbb161fb9f77eaf19e4b5f90e97c2bb0455fd6a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-05 16:57:32 -0500

    Day45 Class

---
+++ src/day45_Exceptions/EveryDaysLunchBreak.java
@@ -0,0 +1,19 @@
+package day45_Exceptions;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class EveryDaysLunchBreak {
+
+    public static void main(String[] args) {
+
+        LocalTime currentTime = LocalTime.now();
+        LocalTime lunchTime = LocalTime.of(1,0);
+
+        if(currentTime.equals(lunchTime)){
+
+        }
+
+    }
+
+}
