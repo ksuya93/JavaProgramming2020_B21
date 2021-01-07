2fda183a44a42b793dd102c76b0668790774dd99
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-07 12:56:37 -0500

    Day46 Class

--- src/day46_JavaRecap/ShapeTask/Shape.java
+++ src/day46_JavaRecap/ShapeTask/Shape.java
@@ -31,8 +31,8 @@ public class Shape {
     public String toString() {
         return "Shape{" +
                 "name= " + name +
-                ", area= "+area()+
-                ", perimeter= "+perimeter()+
+                ", area= "+area()+"cm "+
+                ", perimeter= "+perimeter()+"cm "+
                 '}';
     }
 
