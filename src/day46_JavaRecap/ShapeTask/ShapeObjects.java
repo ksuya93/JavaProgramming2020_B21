d7ff318c166f5d8ccfc251814e643eb5d41456ec
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-07 12:59:52 -0500

    Day46 Class

--- src/day46_JavaRecap/ShapeTask/ShapeObjects.java
+++ src/day46_JavaRecap/ShapeTask/ShapeObjects.java
@@ -3,7 +3,7 @@ package day46_JavaRecap.ShapeTask;
 public class ShapeObjects {
     public static void main(String[] args) {
 
-       Circle circle1 = new Circle(3);
+       Circle circle1 = new Circle(-1);
 
         System.out.println(circle1);
         System.out.println(circle1.getR());
