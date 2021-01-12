a93914b8425ef81f099b33c2b88332dd6141f9b4
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-12 16:11:38 -0500

    Day47 Class

--- src/day47_Abstraction/EmployeeTask/Tester.java
+++ src/day47_Abstraction/EmployeeTask/Tester.java
@@ -1,6 +1,6 @@
 package day47_Abstraction.EmployeeTask;
 
-public class Tester extends Employee {
+public final class Tester extends Employee {
 
     public Tester(String name, int age, char gender, String jobTitle, String ID, double salary) {
         super(name, age, gender, jobTitle, ID, salary);
