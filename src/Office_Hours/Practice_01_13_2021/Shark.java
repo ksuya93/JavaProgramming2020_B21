4dacd327c7f7796d272e9c491c4183b1508d96b8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-13 15:19:14 -0500

    Office Hour: 1/13/2021

--- src/Office_Hours/Practice_01_13_2021/Shark.java
+++ src/Office_Hours/Practice_01_13_2021/Shark.java
@@ -1,6 +1,6 @@
 package Office_Hours.Practice_01_13_2021;
 
-public class Shark extends Animal {
+public final class Shark extends Animal {
     public Shark(String name, String breed, String size, char gender, int age) {
         super(name, breed, size, gender, age);
     }
