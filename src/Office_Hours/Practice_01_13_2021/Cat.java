a86a01fd3e6a8352a16ada1fbf0894b58ceead51
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-13 15:08:51 -0500

    Office Hour: 1/13/2021

---
+++ src/Office_Hours/Practice_01_13_2021/Cat.java
@@ -0,0 +1,17 @@
+package Office_Hours.Practice_01_13_2021;
+
+public final class Cat extends Animal {
+    public Cat(String name, String breed, String size, char gender, int age) {
+        super(name, breed, size, gender, age);
+    }
+
+    @Override
+    protected void speak() {
+        System.out.println("Meow Meow");
+    }
+
+    @Override
+    public String toString() {
+        return "Cat"+super.toString();
+    }
+}
--- src/Office_Hours/Practice_01_13_2021/Dog.java
+++ src/Office_Hours/Practice_01_13_2021/Dog.java
@@ -1,6 +1,6 @@
 package Office_Hours.Practice_01_13_2021;
 
-public class Dog extends Animal {
+public final class Dog extends Animal {
 
     public Dog(String name, String breed, String size, char gender, int age) {
         super(name, breed, size, gender, age);
