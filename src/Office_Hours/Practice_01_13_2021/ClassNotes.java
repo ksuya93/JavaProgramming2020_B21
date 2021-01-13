c051008da5e9fea3a1bad3afab62871992986c0e
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-13 15:36:35 -0500

    Office Hour: 1/13/2021

---
+++ src/Office_Hours/Practice_01_13_2021/ClassNotes.java
@@ -0,0 +1,64 @@
+package Office_Hours.Practice_01_13_2021;
+
+public class ClassNotes {
+    /*
+    01/13/2021
+Practice Topics
+		OOP Inheritance
+		OOP Abstraction
+
+
+Package name: Practice_01_13_2021
+
+
+
+OOP:
+
+	Inheritance: createing IS A relation (sub & super)
+
+	abstraction: hiding implementation details
+
+			abstract method: without { }
+							meant to be overriden, private, final, static can't abstract method
+
+			can only be achived by using:
+								1. abstract class: cannot have object. meant to be super class
+								2. Interface
+
+
+
+
+ZooTask:
+
+	abstract Animal:
+		breed, size, age
+		eat();
+		speak()
+
+	Linon
+		eat(){
+			eat 3 times in a day
+		}
+
+	Tiger
+		eat(){
+			eat 4 times in a day
+			}
+
+	Eagle
+		eat(){
+			eat 1 times in a day
+		}
+
+	Bear
+		eat(){
+			eat 2 times in a day
+			}
+
+	Wolf
+		eat(){
+			eat 5 times in a day
+		}
+
+     */
+}
