ec9aa706d65212f42ef1e026c7bca78de81d6590
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-14 11:53:32 -0500

    Day48 class

--- src/day48_Abstraction/AnimalTask/Dog.java
+++ src/day48_Abstraction/AnimalTask/Dog.java
@@ -1,14 +1,19 @@
 package day48_Abstraction.AnimalTask;
 
-public class Dog extends Animal {
+public class Dog extends Animal implements Playable {
     public Dog(String name, String breed, String size, char gender, int age) {
         super(name, breed, size, gender, age);
     }
 
     @Override
     public void sleep() {
-
+        System.out.println("Dog sleeps 8 hours");
     }
 
+
+    @Override
+    public void play() {
+        System.out.println("Dog plays with with ball");
+    }
 
 }
