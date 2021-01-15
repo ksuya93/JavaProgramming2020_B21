8435793c41c4c7da95511a74f932fe882275ed87
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-15 16:33:16 -0500

    Day49 class

--- src/day49_Abstraction/ShapeTask/ShapeObjects.java
+++ src/day49_Abstraction/ShapeTask/ShapeObjects.java
@@ -58,13 +58,31 @@ public class ShapeObjects {
         }
 */
 
-        circles.forEach( each -> { if(each.area()> 400) System.out.println(each);   } );  // in lambda: the variable is immutable
+        circles.forEach( each -> { if(each.area()> 400) System.out.println(each);   } );  // in lambda: the variable is final (immutable)
 
         System.out.println("------------------------------------------------------------------------");
 
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+       // list.forEach( p ->  { if(p%2 ==0) System.out.println(p);    } );
+        list.forEach( p ->  {    } );
+
+        System.out.println(list);
+
+        System.out.println("-----------------------------------------------------------------------");
+        ArrayList<Integer> oddNumbers = new ArrayList<>();
+        ArrayList<Integer> evenNumbers = new ArrayList<>();
+        ArrayList<Integer> greaterThan5 = new ArrayList<>();
+
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12));
+        numbers.forEach( p ->  { if(p%2==0) evenNumbers.add(p); else oddNumbers.add(p);  }  );
+
+        numbers.forEach( p -> { if(p>5) greaterThan5.add(p);  }  );
 
 
+
+        System.out.println("oddNumbers = " + oddNumbers);
+        System.out.println("evenNumbers = " + evenNumbers);
+        System.out.println("greaterThan5 = " + greaterThan5);
 
 
     }
