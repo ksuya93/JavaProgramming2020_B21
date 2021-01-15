683f06f5a313487db9b553040cfbec77080a4936
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-15 16:18:41 -0500

    Day49 class

--- src/day49_Abstraction/ShapeTask/ShapeObjects.java
+++ src/day49_Abstraction/ShapeTask/ShapeObjects.java
@@ -1,5 +1,8 @@
 package day49_Abstraction.ShapeTask;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ShapeObjects {
     public static void main(String[] args) {
         Circle circle = new Circle(23);
@@ -37,7 +40,29 @@ public class ShapeObjects {
 
         System.out.println("---------------------------------------------------------------------");
 
+        ArrayList<Shape> circles = new ArrayList<>();
+        circles.addAll( Arrays.asList(shapes)  );
+        circles.removeIf( p -> !p.name.equals("Circle"));
 
+        System.out.println(circles.size());
+        System.out.println("---------------------------------------------------------------------");
+
+        circles.forEach( p -> { System.out.println(p); }   );
+
+        System.out.println("--------------------------------------------------------------------");
+/*
+        for(Shape eachShape :  circles){
+            if(eachShape.area() > 400){
+                System.out.println(eachShape);
+            }
+        }
+*/
+
+        circles.forEach( each -> { if(each.area()> 400) System.out.println(each);   } );  // in lambda: the variable is immutable
+
+        System.out.println("------------------------------------------------------------------------");
+
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
 
 
 
