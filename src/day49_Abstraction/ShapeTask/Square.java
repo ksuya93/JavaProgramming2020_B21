e0a6d56975c1749d957e74b9b982bd63809cdaea
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-15 10:49:43 -0500

    Day49 class

--- src/day49_Abstraction/ShapeTask/Square.java
+++ src/day49_Abstraction/ShapeTask/Square.java
@@ -6,17 +6,20 @@ public class Square extends Shape{
 
     public Square(double side) {
         super("Square");
+        if(side <= 0){
+            throw new RuntimeException("No such a square with side of "+side);
+        }
         this.side = side;
     }
 
 
     @Override
     public double area() {
-        return 0;
+        return side * side;
     }
 
     @Override
     public double perimeter() {
-        return 0;
+        return side * 4;
     }
 }
