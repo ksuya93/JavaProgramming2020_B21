d4c58ad720df851c7d7b04e87b704d61b44fcf05
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-15 10:32:48 -0500

    Day49 class

---
+++ src/day49_Abstraction/ShapeTask/Shape.java
@@ -0,0 +1,24 @@
+package day49_Abstraction.ShapeTask;
+
+public abstract class Shape {
+
+    public final String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public abstract double area();
+    public abstract double perimeter();
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name= '" + name + '\'' +
+                ", area= '" + area() + '\'' +
+                ", perimeter= '" + perimeter() + '\'' +
+                '}';
+    }
+
+
+}
