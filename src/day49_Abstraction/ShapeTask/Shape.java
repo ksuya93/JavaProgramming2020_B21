4c233f48392d4b85963d5ac115972b3d190648d2
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-15 11:48:30 -0500

    Day49 class

--- src/day49_Abstraction/ShapeTask/Shape.java
+++ src/day49_Abstraction/ShapeTask/Shape.java
@@ -1,8 +1,11 @@
 package day49_Abstraction.ShapeTask;
 
+import java.text.DecimalFormat;
+
 public abstract class Shape {
 
     public final String name;
+    public static DecimalFormat df = new DecimalFormat("0.00");
 
     public Shape(String name) {
         if(name.isEmpty()){
