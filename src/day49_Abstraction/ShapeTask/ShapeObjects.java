22a354b6836bd3f13291f932e6684fbc43c9367e
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-15 16:06:13 -0500

    Day49 class

--- src/day49_Abstraction/ShapeTask/ShapeObjects.java
+++ src/day49_Abstraction/ShapeTask/ShapeObjects.java
@@ -18,6 +18,29 @@ public class ShapeObjects {
 
         System.out.println(rectangle.getW());
 
+        System.out.println("----------------------------------------------");
+       // Shape shape1 = circle;
+       // Shape shape2 = rectangle;
+       // Shape shape3 = cylinder;
+       // Shape shape4 = cube;
+       // Shape shape5 = square;
+
+        Shape[] shapes = {circle, rectangle, cube, cylinder, square, new Circle(5), new Square(3),
+            new Circle(10), new Circle(15)};
+
+        for(Shape eachShape   : shapes) {
+            if(eachShape.name.equals("Circle")){
+                System.out.println(eachShape);
+            }
+        }
+
+
+        System.out.println("---------------------------------------------------------------------");
+
+
+
+
+
 
     }
 }
