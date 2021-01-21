5838d09075e81bb5acb9873319122c5011ccd989
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-01-21 10:45:42 -0500

    Day50 class

---
+++ src/day50_Polymorphism/PhoneTask/AndroidApps.java
@@ -0,0 +1,5 @@
+package day50_Polymorphism.PhoneTask;
+
+public interface AndroidApps extends Downloadable{
+        String AppStoreName = "Play Market";
+}
---
+++ src/day50_Polymorphism/PhoneTask/AppleApps.java
@@ -0,0 +1,6 @@
+package day50_Polymorphism.PhoneTask;
+
+public interface AppleApps  extends Downloadable{
+        String AppStoreName = "Apple";
+
+}
